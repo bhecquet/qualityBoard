1ee251c0c643aa17b0a0f1700fee1386fae18e34
tipdu <tidumur@gmail.com>
2017-07-04 11:23:30 +0200

    add link on the Jira statistics part

--- packages/Quality/jobs/Jira/test/test-Jira.js
+++ packages/Quality/jobs/Jira/test/test-Jira.js
@@ -7,17 +7,32 @@ var Jira_SUT = require('../Jira');
 
 var mockedConfig, mockedDependencies;
 
-describe ('Jira test', function(){
+mockedDependencies = {
+			logger: console,
+			request : {
+				get :  function (options,response, cb) {
+					return(null, "<HTML>");
+				}
+			}
+		};
 
+
+
+
+describe ('Jira test', function(){
+	
 	it('Should return the title',function(){
 		mockedConfig = {
 			'widgetTitle' : 'title'
 		};
 
+
+
 		Jira_SUT.onRun(mockedConfig,mockedDependencies,function(err,data){
 			assert.equal(data.title,'title');
 		});
 	});
+
 
 	it('Should return Undefined if no data recieved',function(){
 		mockedConfig = {'title' : 'title'};
@@ -27,4 +42,22 @@ describe ('Jira test', function(){
 		});
 	});
 
+
+
+	describe('Request return',function(err,data){
+		it('Should return ["No information recieved"] if data in wrong shape',function(){
+
+			mockedConfig = {"widgetTitle" : " Jira ",
+					"authName" : "Jira",
+					"project" : "ENVMONITOR",
+					"jiraServer" : "https://jira.infotel.com",
+					"jiraRequest" : "/rest/api/2/search?",
+					"jiraXML" : "/si/jira.issueviews:issue-xml/"}
+
+			Jira_SUT.onRun(mockedConfig,mockedDependencies,function(err,data){
+				assert.deepEqual(data.IssuesList,['No informations recieved']);
+			});
+		});
+
+	});
 });
--- packages/Quality/widgets/Jira/Jira.js
+++ packages/Quality/widgets/Jira/Jira.js
@@ -7,6 +7,15 @@ widget = {
 			$('h2', el).text(data.title);
 		}
 
+		//Adress to the issues by stats
+
+		var majorURL = "https://jira.infotel.com/browse/ENVMONITOR-38?jql=project%20%3D%20ENVMONITOR%20and%20priority%20%3D%20Major";
+		var undefinedURL = "https://jira.infotel.com/browse/ENVMONITOR-37?jql=project%20%3D%20ENVMONITOR%20and%20priority%20%3D%20Undefined";
+		var secondURL = "https://jira.infotel.com/browse/ENVMONITOR-47?jql=project%20%3D%20ENVMONITOR%20and%20priority%20%3D%20Secondaire";
+
+		var toDoURL = "https://jira.infotel.com/browse/ENVMONITOR-61?jql=project%20%3D%20%22Outil%20de%20supervision%20des%20environnements%20de%20validation%22%20%20and%20statusCategory%20%3D%20%22To%20Do%22%20%20";
+		var inProgressURL = "https://jira.infotel.com/browse/ENVMONITOR-57?jql=project%20%3D%20%22Outil%20de%20supervision%20des%20environnements%20de%20validation%22%20%20and%20statusCategory%20%3D%20%22In%20Progress%22%20%20%20";
+		var doneURL = "https://jira.infotel.com/browse/ENVMONITOR-55?jql=project%20%3D%20%22Outil%20de%20supervision%20des%20environnements%20de%20validation%22%20%20and%20statusCategory%20%3D%20Done%20%20%20%20";
 
 		// Functions to sort the different issues
 			//Function to sort the issues by id
@@ -52,16 +61,21 @@ widget = {
 		//Name
 		$name = $('.name',el);
 		$name.empty();
-		$name.append(
-			'<span class="libele"> project : </span> <span class="figure"> ' + data.project
-		);
+		if (data.project){
+			$name.append(
+				'<span class="libele"> project : </span> <span class="figure"> ' + data.project
+			);
+		}
+		
 
 		//Version
 		$version = $('.version',el);
 		$version.empty();
-		$version.append(
-			'<span class="libele"> version : </span> <span class="figure"> ' + data.version
-		);
+		if(data.version){
+			$version.append(
+					'<span class="libele"> version : </span> <span class="figure"> ' + data.version
+				);
+		}
 
 		
 
@@ -69,28 +83,37 @@ widget = {
 
 		$iNb= $('.IssueNumber',el);
 		$iNb.empty();
-		$iNb.append(
-			'<span class="libele"> Number of issues : </span> ' + 
-			'<span class="figure">' + data.nbIssues + '</span>'
-		);
+
+		if(data.nbIssues){
+			$iNb.append(
+					'<span class="libele"> Number of issues : </span> ' + 
+					'<span class="figure">' + data.nbIssues + '</span>'
+				);
+		}
 
 		//Stat on the issues
 			//Priorities
 		$Pri=$('.Priorities',el);
 		$Pri.empty();
-		$Pri.append(
-			'<div class="libele"> Major : <span class=figure>' + data.nbMajeur + '</span></div>'+
-			'<div class="libele"> Second : <span class=figure>' + data.nbSecond + '</span></div>'+
-			'<div class="libele"> Undefined : <span class=figure>' + data.nbUndefined + '</span></div>'
-		);
+
+		if(data.nbMajeur){
+			$Pri.append(
+					'<a href="' + majorURL +'"><div class="libele"> Major : <span class=figure>' + data.nbMajeur + '</span></div></a>'+
+					'<a href="' + secondURL +'"><div class="libele"> Second : <span class=figure>' + data.nbSecond + '</span></div>'+
+					'<a href="' + undefinedURL +'"><div class="libele"> Undefined : <span class=figure>' + data.nbUndefined + '</span></div>'
+				);
+		}
 			//Status
 		$Sta = $('.Status',el);
 		$Sta.empty();
-		$Sta.append(
-			'<div class="libele"> Open : <span class=figure>' + data.nbOpen + '</span></div>'+
-			'<div class="libele"> In Progress : <span class=figure>' + data.nbInProcess + '</span></div>'+
-			'<div class="libele"> Done : <span class=figure>' + data.nbDone + '</span></div>'
-		);
+
+		if(data.nbOpen){
+			$Sta.append(
+					'<a href="' + toDoURL +'"><div class="libele"> Open : <span class=figure>' + data.nbOpen + '</span></div>'+
+					'<a href="' + inProgressURL +'"><div class="libele"> In Progress : <span class=figure>' + data.nbInProcess + '</span></div>'+
+					'<a href="' + doneURL +'"><div class="libele"> Done : <span class=figure>' + data.nbDone + '</span></div>'
+				);
+		}
 
 		//IssuesList
 		$IssuesList = $('.IssuesList',el);
@@ -104,44 +127,50 @@ widget = {
 			'<th class="title"> title </th> ' +
 		'</tr>');
 
+		console.log(data.IssuesList);
+
+		if(data.IssuesList.length > 1){
 		var issues = data.IssuesList.sort(idSort).sort(priorSort);
 		var prior = "";
-		console.log($IssuesList);
-		issues.forEach(function(issue){
-			prior = "";
-			//The background of the issue will depands of its priority
-			switch(issue.priority){
-				case 'Undefined':
-					prior = "undefined";
-					break;
-				case 'Majeure':
-					prior = 'major';
-					break;
-				case 'Secondaire':
-					prior = 'second';
-					break;
-				default:
-					prior = 'error';
-					break;
-			}
-			//Get all the informations
-			var id = issue.id;
-			var priority = issue.priority;
-			var status = issue.status;
-			var type = issue.type;
-			var title = issue.title;
-			var adress = data.jiraServer + '/projects/' + data.project + 
-						'/issues/' + id;//url of the real issue on Jira
-			
-						//IssuesTab is the tab of the issues, it will be show on the widget
-			$IssuesList.append(
-						'<tr class="' + prior +'"> ' + 
-							'<th class="priority">   ' + priority + '</th>' +
-							'<th class="status">   ' + status + '</th>' + 
-							'<th class ="type">   ' + type + '</th>' +
-							'<th class="title"> <a href="' + adress +'">' + title + '</a></th>' +//Link to the issue on Jira
-						'</tr>'
-					);			
-		});
+			issues.forEach(function(issue){
+					prior = "";
+					//The background of the issue will depands of its priority
+					switch(issue.priority){
+						case 'Undefined':
+							prior = "undefined";
+							break;
+						case 'Majeure':
+							prior = 'major';
+							break;
+						case 'Secondaire':
+							prior = 'second';
+							break;
+						default:
+							prior = 'error';
+							break;
+					}
+					//Get all the informations
+					var id = issue.id;
+					var priority = issue.priority;
+					var status = issue.status;
+					var type = issue.type;
+					var title = issue.title;
+					var adress = data.jiraServer + '/projects/' + data.project + 
+								'/issues/' + id;//url of the real issue on Jira
+					
+								//IssuesTab is the tab of the issues, it will be show on the widget
+					$IssuesList.append(
+								'<tr class="' + prior +'"> ' + 
+									'<th class="priority">   ' + priority + '</th>' +
+									'<th class="status">   ' + status + '</th>' + 
+									'<th class ="type">   ' + type + '</th>' +
+									'<th class="title"> <a href="' + adress +'">' + title + '</a></th>' +//Link to the issue on Jira
+								'</tr>'
+							);			
+				});
+		} else{
+			$IssuesList.empty();
+			$IssuesList.append('<span class="error">' + data.IssuesList + '</span>');
+		}
 	}
 };
