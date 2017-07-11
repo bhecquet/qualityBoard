3d875a67b95a0a5828307ee37e04ab46fd381999
tipdu <tidumur@gmail.com>
2017-07-11 10:01:26 +0200

    new Jenkins widget and css update

---
+++ packages/Quality/jobs/Jenkins/Jenkins.js
@@ -0,0 +1,14 @@
+module.exports = {
+
+
+	onInit: function (config, dependencies) {
+
+	},
+
+	onRun: function (config, dependencies, jobCallback) {
+
+
+
+		jobCallback(null, {title: config.widgetTitle});
+	}
+};
