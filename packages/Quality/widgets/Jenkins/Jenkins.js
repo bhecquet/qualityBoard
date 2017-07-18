ee205fc7fca8ba75b7947fc0a2580af71240abea
tipdu <tidumur@gmail.com>
2017-07-18 14:14:29 +0200

    Jenkins widget : inline display

--- packages/Quality/widgets/Jenkins/Jenkins.js
+++ packages/Quality/widgets/Jenkins/Jenkins.js
@@ -12,9 +12,16 @@ widget = {
 			else if (job1.name == job2.name) return 0;
 			else return 1;
 		}
+	
+	//Function to convert milliseconds to minutes
+		function millisToMinutesAndSeconds(millis) {
+			var minutes = Math.floor(millis / 60000);
+			var seconds = ((millis % 60000) / 1000).toFixed(0);
+			return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+		}
 
 	//The main container of the widget
-		var $content = $('.content', el);
+		var $content = $('.table', el);
 		$content.empty();
 		var jobInfo = data.jobList.sort(nameSort);
 		var jobLink = data.jenkinsServer + '/job/';
@@ -25,10 +32,11 @@ widget = {
 				var jobPersoLink = jobLink + job.name + '/';
 				if(job.status == 'none'){
 					var jobInfoHTML = 
-						`<div class="job">
-							 <div class="name"> <a href = "${jobPersoLink}"> ${job.name} </a></div>
-							 <div class="error"> No build found </div>
-						</div>`
+						`<tr class="job">
+							 <th class="name"> <a href = "${jobPersoLink}"> ${job.name} </a></th>
+							 <th class="error"> No build found </th>
+							 <th class="empty"> </th>
+						</tr>`
 					;
 				}
 				else if(job.status == 'authenticationError'){
@@ -37,15 +45,32 @@ widget = {
 					;
 				}
 				else{
-					var jobInfoHTML = 
+					var resultImage = '';
+					switch(job.result){
+						case 'SUCCESS':
+							resultImage = '<img src="/widgets/resources?resource=Quality/Jenkins/SuccessIcon.png">';
+							break;
+						case 'FAILURE':
+							resultImage = '<img src="/widgets/resources?resource=Quality/Jenkins/FailureIcon.png">';
+							break;
+						default:
+							resultImage = '<img src="/widgets/resources?resource=Quality/Jenkins/MissingIcon.png">';
+					}
+					/*var jobInfoHTML = 
 						`<div class="job">
-							<div class="name"> <a href = "${jobPersoLink}"> ${job.name} </a></div>
-							 <div class="description"> 
-							 	<span class = "title"> Duration  </span><span class = "figure"> ${job.duration} </span>
-							 	<span class = "title"> Success  </span><span class = "${job.result}"> ${job.result} </span>
-							 </div>
-							 <div class="status"> <span class = "title">Status</span> <span class="${job.status}">${job.status} </span> </div>
+							<span class="name"> <a href = "${jobPersoLink}"> ${job.name} </a></span>
+							 <span class="description"> 
+							 	<span class = "title"> Duration  </span><span class = "figure"> ${millisToMinutesAndSeconds(job.duration)} </span>
+							 	<span class = "title">  </span><span class = "${job.result}"> ${resultImage} </span>
+							 </span>
 						</div>`
+					;*/
+					var jobInfoHTML =
+						`<tr class="job">
+							<th class="name"> <a href = "${jobPersoLink}"> ${job.name} </a> </th>
+							<th class="duration"><span class = "title"> Duration  </span><span class = "figure"> ${millisToMinutesAndSeconds(job.duration)} </span></th>
+							<th class="status">${resultImage} </th>
+						</tr>`
 					;
 				}
 				$content.append(jobInfoHTML);
